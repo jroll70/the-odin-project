f1ed33da8d528ca5ff001a1d5dbadf190ec06f8f
jroll70 <jonryan42@gmail.com>
2026-03-16 15:51:00 -0600

    Adding the javascript directories and files

---
+++ foundations/javascript-basics/variables-operators/javascript.js
@@ -0,0 +1,19 @@
+let fisrtName = "John";
+let lastName = "Doe";
+
+console.log(fisrtName);
+console.log(lastName);
+
+let age = 11;
+console.log(age)
+
+age = 54
+console.log(age)
+
+const pi = 3.14;
+// Cant reassign constants
+// pi = 10;
+
+console.log(pi);
+
+console.log(23 + 97)
