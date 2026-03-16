89a42679538c15e3143eb4bf2c325c5b02d2e658
jroll70 <jonryan42@gmail.com>
2026-03-16 15:56:08 -0600

    Finishing up the first lesson

--- foundations/javascript-basics/variables-operators/javascript.js
+++ foundations/javascript-basics/variables-operators/javascript.js
@@ -17,3 +17,21 @@ const pi = 3.14;
 console.log(pi);
 
 console.log(23 + 97)
+
+console.log(1 + 2 + 3 + 4 + 5 + 6)
+
+console.log((4 + 6 + 9) / 77 )
+
+let a = 10
+console.log(a)
+
+a = 29
+console.log(a)
+
+b = 7 * a
+console.log(b)
+
+const max = 57
+const actual = max - 13
+const percentage = actual / max
+console.log(percentage)
